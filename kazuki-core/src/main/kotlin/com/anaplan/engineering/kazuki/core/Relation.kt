1965c430dbb45338c3ee236eb577f26a98d30fd0
simon-fraser <simon.fraser@anaplan.com>
2025-05-16 15:30:23 +0100

    Ensure correct filter is used in dom/range ops

--- kazuki-core/src/main/kotlin/com/anaplan/engineering/kazuki/core/Relation.kt
+++ kazuki-core/src/main/kotlin/com/anaplan/engineering/kazuki/core/Relation.kt
@@ -4,6 +4,8 @@ import com.anaplan.engineering.kazuki.core.internal.__KRelation
 import com.anaplan.engineering.kazuki.core.internal.__KSet
 import com.anaplan.engineering.kazuki.core.internal.transformSet
 import java.util.Collections.addAll
+import kotlin.collections.filter as kotlinFilter
+
 
 interface Relation<D, R> : Set<Tuple2<D, R>> {
 
@@ -21,19 +23,19 @@ infix operator fun <D, R, T : Relation<D, R>> T.plus(t: Tuple2<D, R>) = transfor
 
 infix operator fun <D, R, T : Relation<D, R>> T.plus(m: Relation<D, R>) = transformSet { it.elements + m }
 
-infix fun <D, R, T : Relation<D, R>> T.domRestrictTo(s: Set<D>) = transformSet { it.elements.filter { (k, _) -> k in s } }
+infix fun <D, R, T : Relation<D, R>> T.domRestrictTo(s: Set<D>) = transformSet { it.elements.kotlinFilter { (k, _) -> k in s } }
 
 infix fun <D, R, T : Relation<D, R>> T.drt(s: Set<D>) = domRestrictTo(s)
 
-infix fun <D, R, T : Relation<D, R>> T.rngRestrictTo(s: Set<R>) = transformSet { it.elements.filter { (_, v) -> v in s } }
+infix fun <D, R, T : Relation<D, R>> T.rngRestrictTo(s: Set<R>) = transformSet { it.elements.kotlinFilter { (_, v) -> v in s } }
 
 infix fun <D, R, T : Relation<D, R>> T.rrt(s: Set<R>) = rngRestrictTo(s)
 
-infix fun <D, R, T : Relation<D, R>> T.domSubtract(s: Set<D>) = transformSet { it.elements.filter { (k, _) -> k !in s } }
+infix fun <D, R, T : Relation<D, R>> T.domSubtract(s: Set<D>) = transformSet { it.elements.kotlinFilter { (k, _) -> k !in s } }
 
 infix fun <D, R, T : Relation<D, R>> T.dsub(s: Set<D>) = domSubtract(s)
 
-infix fun <D, R, T : Relation<D, R>> T.rngSubtract(s: Set<R>) = transformSet { it.elements.filter { (_, v) -> v !in s } }
+infix fun <D, R, T : Relation<D, R>> T.rngSubtract(s: Set<R>) = transformSet { it.elements.kotlinFilter { (_, v) -> v !in s } }
 
 infix fun <D, R, T : Relation<D, R>> T.rsub(s: Set<R>) = rngSubtract(s)
 
